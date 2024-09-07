d582041dff1797812ec60cc00058b922ac01b391
Rohit <rspatil4545@gmail.com>
2024-09-07 20:56:42 +0530

    git file

---
+++ main.py
@@ -0,0 +1,31 @@
+#streamlit UI 
+
+import streamlit as st
+from PIL import Image
+from src.tamper import tamper_detection
+
+
+def main():
+    st.title("Image Tampering Detection")
+    
+    uploaded_file = st.file_uploader("Upload Image...", type=["jpg", "jpeg", "png"])
+    
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+        input_image_name = uploaded_file.name
+        saved_file_path = tamper_detection.save_uploaded_file(uploaded_file)
+
+        if st.button("Annotate Image"):
+            annotated_img, output_path = tamper_detection.annotate_and_save_image(saved_file_path, input_image_name)
+            if annotated_img is not None:
+                st.success("Process complete")
+                st.image(annotated_img, caption='Annotated Image', use_column_width=True)
+
+                st.success(f"Annotated image saved to {output_path}")
+            else:
+                st.warning("No tampering detected in the image.")
+
+if __name__ == "__main__":
+    main()
---
+++ src/tamper.py
@@ -0,0 +1,68 @@
+
+
+from ultralytics import YOLO
+import cv2
+import os
+import streamlit as st
+
+save_folder = "upload_files"
+os.makedirs(save_folder,exist_ok=True)
+output_folder = "output"
+os.makedirs(output_folder, exist_ok=True)
+
+class_colors = {
+    0: (0, 0, 255),   # Red for 'Whitener'
+    1: (0, 255, 0),   # Green for 'Overwriting'
+    # 2: (255, 0, 0),   # Blue for 'Re-writing'
+    2 :(0, 0, 255), 
+    3: (0, 255, 255)  # Yellow for 'Erase'
+}
+
+class tamper_detection:
+
+    def save_uploaded_file(uploaded_file):
+        # os.makedirs(save_folder, exist_ok=True)
+        file_path = os.path.join(save_folder, uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        return file_path
+
+    def annotate_and_save_image(input_image_path, output_image_name):
+        # Initialize the progress bar in Streamlit
+
+        st.write ("Tamper Cheaking in progress...")
+        progress_bar = st.progress(0)
+        
+        model = YOLO("tamper_model.pt")
+        results = model.predict(input_image_path)
+
+        if results[0].boxes is not None:
+            tampering_classes = [0, 1, 2, 3]
+            annotated_img = results[0].orig_img.copy()
+
+            total_boxes = len(results[0].boxes)  
+            progress_increment = 1 / total_boxes if total_boxes > 0 else 1
+
+            for idx, box in enumerate(results[0].boxes):
+                cls = int(box.cls.item())
+                if cls in tampering_classes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+                    label = results[0].names[cls]
+                    color = class_colors.get(cls, (255, 255, 255))
+                    cv2.rectangle(annotated_img, (x1, y1), (x2, y2), color, 6)
+                    cv2.putText(annotated_img, f'{label}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 2.5, color, 6)
+                
+                # Update the progress bar after each box is processed
+                progress_bar.progress(min((idx + 1) * progress_increment, 1.0))
+            
+            # os.makedirs("output", exist_ok=True)
+            output_image_path = os.path.join(output_folder, output_image_name)
+            cv2.imwrite(output_image_path, annotated_img)
+            
+            # Indicate completion of progress bar
+            progress_bar.progress(1.0)
+            
+            return annotated_img, output_image_path
+        else:
+            progress_bar.progress(1.0)  # If no boxes were found, just complete the progress bar
+            return None, None
